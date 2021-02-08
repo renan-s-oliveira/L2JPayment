35c49bed8403781f14b188a755b78b72815c5668
Renan de Souza Oliveira <renanoliveira1988@gmail.com>
2021-02-08 20:32:28 -0300

    Secound commit

---
+++ src/Providers/Controllers/OrderController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace L2JPayment\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+
+class CallbackController extends BaseController
+{
+    public function order(Request $request)
+    {
+        
+    }
+
+}
